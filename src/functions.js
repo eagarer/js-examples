28788b460427900e9c07a1f7d521754cfb3a3079
eagarer <erayagarer@gmail.com>
2022-08-28 11:11:26 +0300

    first commit

---
+++ src/functions.js
@@ -0,0 +1,8 @@
+function addToCart(quantity, productName="Elma") {
+    console.log("Sepete eklendi : ürün :" + productName + " adet " + quantity) 
+}
+
+//addToCart()
+addToCart("Yumurta",10)
+addToCart(10)
+//addToCart("Karpuz")
